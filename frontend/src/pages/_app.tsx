3b32181eb505a12a62ab4e06a9062dbd58f901a0
Eric Thomas <bluebird6922@gmail.com>
2022-10-31 11:04:39 +0300

    theme

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
+import { Toaster } from 'react-hot-toast';
 import { CacheProvider } from "@emotion/react";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
 import { createEmotionCache } from "../utils/create-emotion-cache";
 import { createTheme } from "../theme";
 
@@ -16,6 +18,8 @@ export default function App({ Component, pageProps }: AppProps) {
           mode: "dark",
         })}
       >
+        <CssBaseline />
+        <Toaster position="top-center" />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
