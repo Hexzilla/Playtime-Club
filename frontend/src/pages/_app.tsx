df4d4423a70b7a0143446c5b6fb02fa7c9dcb1f0
Eric Thomas <bluebird6922@gmail.com>
2022-10-31 11:27:24 +0300

    play component

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,27 +1,46 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
-import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from "@mui/material/styles";
+import { CacheProvider } from '@emotion/react';
+import { StyledEngineProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createEmotionCache } from "../utils/create-emotion-cache";
-import { createTheme } from "../theme";
+import { createEmotionCache } from '../utils/create-emotion-cache';
+import { createTheme } from '../theme';
+import { ContextProvider } from '../contexts/ContextProvider';
 
 const clientSideEmotionCache = createEmotionCache();
 
-export default function App({ Component, pageProps }: AppProps) {
+interface MyAppProps {
+  Component: AppProps['Component'] & {
+    getLayout: (children: any) => any;
+  };
+  pageProps: AppProps['pageProps'];
+}
+
+const App = (props: MyAppProps) => {
+  const { Component, pageProps } = props;
+
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <ThemeProvider
-        theme={createTheme({
-          direction: "ltr",
-          responsiveFontSizes: true,
-          mode: "dark",
-        })}
-      >
-        <CssBaseline />
-        <Toaster position="top-center" />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider
+          theme={createTheme({
+            direction: 'ltr',
+            responsiveFontSizes: true,
+            mode: 'dark',
+          })}
+        >
+          <CssBaseline />
+          <Toaster position="top-center" />
+          <ContextProvider>
+            {getLayout(<Component {...pageProps} />)}
+          </ContextProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
     </CacheProvider>
   );
-}
+};
+
+export default App;
