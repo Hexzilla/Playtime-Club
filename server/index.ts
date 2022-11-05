90fccbd8603af376704f0a82d8413a817b5faa6e
Eric Thomas <bluebird6922@gmail.com>
2022-11-05 09:58:52 +0300

    add logs

--- server/index.ts
+++ server/index.ts
@@ -5,19 +5,27 @@ import cors from './cors';
 import socketServer from "./server";
 
 const dev = process.env.APP_ENV !== 'production';
+console.log('~~~~~~~~~~~~~~~~1')
 const app = next({ dev });
+console.log('~~~~~~~~~~~~~~~~2')
 const handle = app.getRequestHandler();
+console.log('~~~~~~~~~~~~~~~~3')
 const port = process.env.PORT || 3000;
+console.log('~~~~~~~~~~~~~~~~4', port)
 
 app.prepare().then(() => {
+  console.log('~~~~~~~~~~~~~~~~10')
   const server: Express = express();
 
+  console.log('~~~~~~~~~~~~~~~~11')
   server.use(cors);
 
+  console.log('~~~~~~~~~~~~~~~~12')
   server.get('*', (req: Request, res: Response) => {
     return handle(req, res);
   });
 
+  console.log('~~~~~~~~~~~~~~~~13')
   /// import socketio communication module
   const http = require("http").Server(server);
   const io = new Server(http, {
@@ -27,9 +35,11 @@ app.prepare().then(() => {
     },
   });
 
+  console.log('~~~~~~~~~~~~~~~~14')
   /// Open a connection with the specific client
   io.on("connection", socketServer);
 
+  console.log('~~~~~~~~~~~~~~~~15')
   http.listen(port, () => {
     console.log('Server is running...')
   });
