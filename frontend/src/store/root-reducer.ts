8e062a94ff370958e38d66fa8a65b22ecc523254
Eric Thomas <bluebird6922@gmail.com>
2022-11-07 10:48:49 +0300

    delete old codes

--- frontend/src/store/root-reducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { reducer as playReducer } from '../slices/play';
-
-export const rootReducer = combineReducers({
-  play: playReducer,
-});
