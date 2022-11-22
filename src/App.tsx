50582bff8ac6391442f15b2635b0f6e51fc962cf
Eric Thomas <bluebird6922@gmail.com>
2022-11-22 11:42:50 +0300

    add countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 import Play from './pages/play';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route index element={<Play />} />
+      <Route index element={<Home />} />
       <Route path="play" element={<Play />} />
     </Routes>
   </BrowserRouter>
